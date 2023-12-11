2dbc188855380d2b1d9df430cae76f3a5de6b84c
장민규 <a90618693@gmail.com>
2023-12-11 16:37:11 +0900

    [level 2] Title: 프로세스, Time: 1.99 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cs
+++ 프로그래머스/2/42587. 프로세스/프로세스.cs
@@ -1,48 +1,43 @@
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-public class Solution 
-{
-    public int solution(int[] priorities, int location) 
+public class Solution {
+    public int solution(int[] priorities, int location)
     {
-            int answer = 0;
+        int answer = 0;
 
-            int maxRank = priorities.Max();
+        var queue = new Queue<KeyValuePair<int, int>>();
 
-            Dictionary<int, int> rankMapDic = new Dictionary<int, int>();
+        for (int i = 0; i < priorities.Count(); i++)
+        {
+            queue.Enqueue(new KeyValuePair<int, int>(i, priorities[i]));
+        }
 
-            for (int i = 0; i < priorities.Count(); i++)
+        int maxRank = queue.Max(i => i.Value);
+
+        while (queue.Count > 0)
+        {
+            answer++;
+
+            var queueItem = queue.Dequeue();//del
+
+            if (queueItem.Value < maxRank)
             {
-                rankMapDic.Add(i, priorities[i]);
+                queue.Enqueue(queueItem);//add
+                answer--;
+
+                continue;
             }
-
-            var queue = new Queue<int>(rankMapDic.Keys);
-
-            while (queue.Count > 0)
+            else if (queueItem.Key == location)
             {
-                answer++;
-
-                int locationKey = queue.Dequeue();//del
-
-                int rank = rankMapDic[locationKey];
-
-                if (rank < maxRank)
-                {
-                    queue.Enqueue(locationKey);//add
-                    answer--;
-
-                    continue;
-                }
-                else if (locationKey == location)
-                {
-                    break;
-                }
-                else if (rank == maxRank && !queue.Any(i => rankMapDic[i] == maxRank))
-                {
-                    maxRank = queue.Max(i => rankMapDic[i]);
-                }
+                break;
             }
+            else if (queueItem.Value == maxRank)
+            {
+                maxRank = queue.Max(i => i.Value);
+            }
+        }
 
-            return answer;
+        return answer;       
     }
 }
