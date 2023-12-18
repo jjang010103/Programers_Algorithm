49bf818c33094c7637da25da1dc823ebb5e3b2c0
장민규 <a90618693@gmail.com>
2023-12-18 18:54:07 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 17.40 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cs
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cs
@@ -2,32 +2,34 @@ using System;
 
 public class Solution 
 {
+    private int Count_0 { get; set; }
+    private int Count_1 { get; set; }
+    
     public int[] solution(int[,] arr) 
     {
-        int count_0 = 0;
-        int count_1 = 0;
+        Count_0 = 0;
+        Count_1 = 0;
 
-        for (int boxIndex = arr.GetLength(0); boxIndex > 0; boxIndex /= 2)//자를 갯수
+        int arrLength = arr.GetLength(0);
+
+        for (int boxIndex = arrLength; boxIndex > 0; boxIndex /= 2)//자를 갯수
         {
-            for (int startIndex_Y = 0 * boxIndex; startIndex_Y < arr.GetLength(0); startIndex_Y += boxIndex)
+            for (int startIndex_Y = 0 * boxIndex; startIndex_Y < arrLength; startIndex_Y += boxIndex)
             {
-                for (int startIndex_X = 0 * boxIndex; startIndex_X < arr.GetLength(0); startIndex_X += boxIndex)
+                for (int startIndex_X = 0 * boxIndex; startIndex_X < arrLength; startIndex_X += boxIndex)
                 {
-                    int numZipIndex = GetWhichNumberZip(startIndex_Y, startIndex_X, boxIndex, arr);
-
-                    if (numZipIndex == 0) count_0++;
-                    else if (numZipIndex == 1) count_1++;
+                    GetWhichNumberZip(startIndex_Y, startIndex_X, boxIndex, arr);
                 }
             }
         }
 
-        return new int[] { count_0, count_1 };
+        return new int[] { Count_0, Count_1 };
     }
     
-    private int GetWhichNumberZip(int startIndex_Y, int startIndex_X, int boxIndex, int[,] arr)
+    private void GetWhichNumberZip(int startIndex_Y, int startIndex_X, int boxIndex, int[,] arr)
     {
-        int count_0 = 0;
-        int count_1 = 0;
+        int tempCount_0 = 0;
+        int tempCount_1 = 0;
 
         for (int k = startIndex_Y; k < startIndex_Y + boxIndex; k++)//Y
         {
@@ -35,24 +37,25 @@ public class Solution
             {
                 if (arr[k, l] == -1)
                 {
-                    return -1;
+                    return;
                 }
                 else
                 {
-                    if (arr[k, l] == 0) count_0++;
-                    else if (arr[k, l] == 1) count_1++;
+                    if (arr[k, l] == 0) tempCount_0++;
+                    else if (arr[k, l] == 1) tempCount_1++;
                 }
             }
         }
 
-        int returnValue = count_0 == boxIndex * boxIndex ? 0 : count_1 == boxIndex * boxIndex ? 1 : -1;
+        bool isCheck = false;
 
-        if (returnValue != -1) CheckZipVal(startIndex_Y, startIndex_X, boxIndex, arr);
+        if (isCheck |= tempCount_0 == 0) Count_1++;
+        else if (isCheck |= tempCount_1 == 0) Count_0++;
 
-        return returnValue;
+        if(isCheck) CheckIsZipVal(startIndex_Y, startIndex_X, boxIndex, arr);
     }
-    
-    private void CheckZipVal(int startIndex_Y, int startIndex_X, int boxIndex, int[,] arr)
+
+    private void CheckIsZipVal(int startIndex_Y, int startIndex_X, int boxIndex, int[,] arr)
     {
         for (int k = startIndex_Y; k < startIndex_Y + boxIndex; k++)//Y
         {
