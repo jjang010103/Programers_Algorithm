1d1242600ccebe9f9b24a5ec02069ed6882ee3fa
장민규 <a90618693@gmail.com>
2023-12-20 01:34:40 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.26 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution 
+{
+    public int solution(int bridge_length, int weight, int[] truck_weights) 
+    {
+        int totalTime = 0;
+
+        int sumWeight = 0;
+
+        Queue<int> trucks = new Queue<int>(truck_weights);
+
+        Queue<int> bridge = new Queue<int>();
+
+        while (trucks.Count != 0 || sumWeight == 0)
+        {
+            if (bridge.Count != 0 && bridge.Count % bridge_length == 0)
+            {
+                sumWeight -= bridge.Dequeue();
+            }
+
+            if (trucks.TryPeek(out int temp) && sumWeight + temp <= weight)
+            {
+                bridge.Enqueue(trucks.Dequeue());
+
+                sumWeight += temp;
+            }
+            else
+            {
+                bridge.Enqueue(0);
+            }
+
+            totalTime++;
+        };
+
+        return totalTime + bridge_length;
+    }
+}
