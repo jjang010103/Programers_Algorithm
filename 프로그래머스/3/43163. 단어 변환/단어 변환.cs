3daac1a074e263830fcb55730df1b5ced11e0a54
장민규 <a90618693@gmail.com>
2023-12-21 21:17:34 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cs
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class Solution 
-{
+public class Solution {
+
     private string[] Words { get; set; }
-    
+
     public int solution(string begin, string target, string[] words) 
     {
         if (!words.Contains(target)) return 0;
@@ -17,20 +17,27 @@ public class Solution
 
         while (!tempValArr.Contains(target))
         {
-            tempValArr = this.ChangeWord(tempValArr);
+            tempValArr = ChangeWord(tempValArr);
             answer++;
         }
 
         return answer;
     }
-    
+
     private IEnumerable<string> ChangeWord(IEnumerable<string> beginArr)
     {
         foreach (string begin in beginArr)
         {
             foreach (string word in this.Words)
             {
-                if (begin.Where((v, i) => begin[i] != word[i]).Count() == 1) yield return word;
+                int count = 0;
+                
+                for(int i = 0; i < begin.Length; i++)
+                {
+                    if (begin[i] != word[i]) count++;
+                }
+
+                if (count == 1) yield return word;
             }
         }
     }
